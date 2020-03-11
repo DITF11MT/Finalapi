39f64b48259a45e4184e8731f088792ff1a9c0d9
Vasili8 <tal7a.tmam+noreplay@gmail.com>
2020-03-11 19:49:48 +0200

    added db config

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
+    host : 'postgresql-graceful-09329',
+    user : 'gkjicbbzsfchok',
     password : '',
-    database : 'finaldb'
+    database : 'hidden-meadow-23107'
   }
 });
 
